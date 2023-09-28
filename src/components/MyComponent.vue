1ec4f8d757d7b159079de6a9ce266756cf4dd42b
Eduardo Tejada <et3858@gmail.com>
2023-09-28 11:16:45 -0300

    VideoCard component: first approach

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import VideoCard from './VideoCard.vue';
 
 defineProps<{ msg: string }>()
 
@@ -9,37 +10,20 @@ const count = ref(0)
 <template>
     <h1>{{ msg }}</h1>
 
-    <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
-
-        <!--
-        <p>
-            Edit
-            <code>components/MyComponent.vue</code> to test HMR
-        </p>
-        -->
+    <div class="videos-container">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
     </div>
-
-
-
-
-    <!--
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-        in your IDE for a better DX
-    </p>
-
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-    -->
 </template>
 
 <style scoped>
-.read-the-docs {
-    color: #888;
+.videos-container {
+    display: flex;
+    flex-flow: wrap;
+    gap: 70px;
+    max-width: 1000px;
 }
 </style>
---
+++ src/components/VideoCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import RemoveButton from "./RemoveButton.vue";
+</script>
+
+<template>
+    <div class="video-card">
+        <RemoveButton />
+    </div>
+</template>
+
+<style scoped>
+.video-card {
+    background-color: gray;
+    flex-shrink: 0;
+    height: 150px;
+    position: relative;
+    width: 263px;
+}
+</style>
