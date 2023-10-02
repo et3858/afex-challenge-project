98db8e53ac3d3e796262591e131354c42b2dfd09
Eduardo Tejada <et3858@gmail.com>
2023-10-02 12:17:35 -0300

    Set query of video resource from the API

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import {
+    onMounted,
+    ref,
+    watch,
+} from 'vue';
 import { useModal } from 'vue-final-modal';
 
 import Header from './Header.vue';
 import VideoCard from './VideoCard.vue';
 import VideoDetailsModal from "./modals/VideoDetailsModal.vue";
 
+import type { Video } from "./../types";
+
 
 const count = ref(0);
 const VIDEO_CARDS = Array.from(Array(5));
+const videos = ref<Video[]>([]);
 
 
 const { open, close } = useModal({
@@ -24,14 +31,45 @@ const { open, close } = useModal({
     },
 });
 
+
+
+
+const fetchVideos = () => {
+    // 
+    const URL = "http://localhost:3000";
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(response => {
+            videos.value = response.data;
+        })
+        .catch(error => {
+            console.warn(error);
+        });
+};
+
+onMounted(() => {
+    fetchVideos();
+});
+
+
+watch(videos, () => {
+    console.log(videos.value)
+});
+
+
 </script>
 
 <template>
     <Header />
 
     <div class="videos-container">
-        <template v-for="(e, key) in VIDEO_CARDS" :key="key">
-            <VideoCard @click="() => open()" />
+        <!-- <template v-for="(e, key) in VIDEO_CARDS" :key="key"> -->
+        <template v-for="video in videos" :key="video.id">
+            <VideoCard
+                :video="video"
+                @click="() => open()"
+            />
         </template>
     </div>
 </template>
--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -2,7 +2,13 @@
 import { useModal } from 'vue-final-modal';
 import RemoveButton from "./RemoveButton.vue";
 import ConfirmModal from "./modals/ConfirmModal.vue";
+import type { Video } from "./../types";
 
+interface IProps {
+    video: Video,
+};
+
+const props = defineProps<IProps>();
 
 const { open, close } = useModal({
     component: ConfirmModal,
@@ -17,18 +23,37 @@ const { open, close } = useModal({
 });
 </script>
 
+<!--
+Youtube video thumbnail aspect ratios:
+
+default  -> 4:3
+high     -> 4:3
+standard -> 4:3
+maxres   -> 16:9
+medium   -> 16:9
+-->
+
 <template>
     <div class="video-card">
         <RemoveButton @click="() => open()" />
+
+        <img :src="props.video.thumbnails.medium.url" :alt="props.video.title" />
     </div>
 </template>
 
 <style scoped>
 .video-card {
     background-color: gray;
+    cursor: pointer;
     flex-shrink: 0;
     height: 150px;
     position: relative;
     width: 263px;
+}
+
+
+.video-card img {
+    height: 100%;
+    width: 100%;
 }
 </style>
